be8b555a48b6c58d791a11f322612084fe23ba81
nitin10 <nitiniszod10@gmail.com>
2026-04-17 13:51:11 +0530

    Revert "Remove Experience section from main page composition"

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,6 +9,7 @@ import HeroSection from '@/components/HeroSection';
 import IntroSection from '@/components/IntroSection';
 import SkillsSection from '@/components/SkillsSection';
 import ProjectsSection from '@/components/ProjectsSection';
+import ExperienceSection from '@/components/ExperienceSection';
 import TechStackSection from '@/components/TechStackSection';
 import RecognitionSection from '@/components/RecognitionSection';
 import ContactSection from '@/components/ContactSection';
@@ -56,6 +57,7 @@ const Index = () => {
           
           <SkillsSection />
           <ProjectsSection />
+          <ExperienceSection />
           <RecognitionSection />
         </div>
         
